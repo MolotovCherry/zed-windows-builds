262c78eb0887f3a5a6943ec6dcf53c5e9e63f3d5
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2025-05-04 17:57:07 -0700

    Add back cmark

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::path::Path;
 use std::{error::Error, io::Write as _};
 
 use crossterm::{event::read, event::Event as CEvent};
+use pulldown_cmark::{Event, HeadingLevel, Options, Parser, Tag, TagEnd};
 use zip::read::ZipArchive;
 
 #[tokio::main]
@@ -60,7 +61,46 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     if let Some(body) = latest.body {
-        println!("\n{}", termimad::term_text(&body));
+        let mut options = Options::empty();
+        options.insert(Options::ENABLE_TABLES);
+        let parser = Parser::new_ext(&body, options);
+        let data = parser
+            .map(|event| match event {
+                Event::Start(t) => match t {
+                    Tag::Heading { level, .. } => match level {
+                        HeadingLevel::H2 => "## ".to_owned(),
+                        h => unimplemented!("heading level unimplemented {h:?}"),
+                    },
+                    Tag::Table(_) => "\n| - | - |".to_owned(),
+                    Tag::TableHead => "\n".to_owned(),
+                    Tag::TableRow => "\n".to_owned(),
+                    Tag::TableCell => "|".to_owned(),
+                    Tag::Link { .. } => "".to_owned(),
+                    Tag::CodeBlock(_) => "```\n".to_owned(),
+                    Tag::Strikethrough => "~~".to_owned(),
+                    Tag::Strong => "**".to_owned(),
+                    _ => unimplemented!("tag unimplemented: {t:?}"),
+                },
+                Event::End(t) => match t {
+                    TagEnd::Table => "".to_owned(),
+                    TagEnd::TableHead => "|".to_owned(),
+                    TagEnd::TableRow => "|".to_owned(),
+                    TagEnd::TableCell => "".to_owned(),
+                    TagEnd::Heading(_) => "".to_owned(),
+                    TagEnd::Link => "".to_owned(),
+                    TagEnd::CodeBlock => "\n```".to_owned(),
+                    TagEnd::Strikethrough => "~~".to_owned(),
+                    TagEnd::Strong => "**".to_owned(),
+                    t => unimplemented!("tagend unimplemented: {t:?}"),
+                },
+                Event::Text(t) => t.to_string(),
+                Event::Code(c) => format!("`{c}`"),
+
+                e => unimplemented!("event unimplemented: {e:?}"),
+            })
+            .collect::<String>();
+
+        println!("\n{}", termimad::term_text(&data));
         pause();
     }
 
